53c25fa637b64bbb922e29504d808a7cffdb1d22
Aaron <aaturner1995@gmail.com>
2023-06-12 11:01:29 -0300

    added resolvers to query and mutate the graphql API

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,61 @@ const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const data = await User.findOne({ _id: context.user._id }).select('-__v -password');
+                return data;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        }
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user)
 
+            return { token, user };
+        },
+        login: async (parent, { email, password}) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No profile with this email found!');
+            }
+
+            const correctPw = await user.isCorrectPassword(password)
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const user = await User.findOneAdnUpdate(
+                    { _id: context.user_id },
+                    { $addToSet: { savedBooks: book }},
+                    { new: True }
+                )
+                return user
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const user = await User.findOneAdnUpdate(
+                    { _id: context.user_id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: True }
+                )
+                return user;
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        }
+    }
 }
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,11 +34,10 @@ const typeDefs = gql`
     }
     type Mutation {
         login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, pasword: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
         saveBook(book: savedBookInput): User
         removeBook(bookId: ID!): User
     }
-    `
-
+`;
 
 module.exports = typeDefs
